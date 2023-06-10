2c33fac14ef440dce61008b49ff67ed7df266d66
Tyler <tylerwertman@outlook.com>
2023-06-10 19:53:42 -0400

    404 image, profile picture styles

--- client/src/components/BrightIdeas.jsx
+++ client/src/components/BrightIdeas.jsx
@@ -273,7 +273,7 @@ const BrightIdeas = (props) => {
                             <div className='mt-5' key={idea._id}>
                                 {
                                     idea?.addedBy ?
-                                        <><span>On {new Date(idea.createdAt).toLocaleString("en-US", options)} at {new Date(idea.createdAt).toLocaleString([], { timeStyle: 'short' })}, </span><Link to={`/users/${idea.addedBy._id}`}>@{idea?.addedBy.displayName}</Link><span> said:</span>&nbsp;</> :
+                                        <><span>On {new Date(idea.createdAt).toLocaleString("en-US", options)} at {new Date(idea.createdAt).toLocaleString([], { timeStyle: 'short' })}, </span><img className="profilePicture" src={`${idea.addedBy?.profilePicture}`} alt="" style={{width:"35px", height:"35px"}}/> <Link to={`/users/${idea.addedBy._id}`}>@{idea?.addedBy.displayName}</Link><span> said:</span>&nbsp;</> :
                                         <span>Deleted User says: </span>
                                 }
                                 <br className="MQHide" />
